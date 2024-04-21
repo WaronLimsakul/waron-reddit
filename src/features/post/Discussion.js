5ecbb6a6a7d7d88b6af4946d08fbe9d59f8f89a5
Waron Limsakul <poonro@gmail.com>
2024-04-21 11:50:44 +0700

    add date the discussion is created

--- src/features/post/Discussion.js
+++ src/features/post/Discussion.js
@@ -1,24 +1,34 @@
-import { Card, Divider, CardHeader, CardContent, Grid, Typography} from "@mui/material";
+import {
+  Card,
+  Divider,
+  CardHeader,
+  CardContent,
+  Grid,
+  Typography,
+} from "@mui/material";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-
+import { TimeAgo } from "./TimeAgo";
 
 export const Discussion = ({ discussion }) => {
-    return (
-        <Card style={{ marginTop: "10" }}>
-            <Divider />
-            <CardHeader
-              avatar={<AccountCircle></AccountCircle>}
-              title={
-                <Grid container alignItems="flex-start">
-                  <Typography variant="subtitle1">{discussion.author}</Typography>
-                </Grid>
-              }
-            />
-            <CardContent>
-              <Grid container alignItems="flex-start">
-                <Typography>{discussion.body}</Typography>
-              </Grid>
-            </CardContent>
-          </Card>
-    )
-}
+  return (
+    <Card style={{ marginTop: "10" }}>
+      <Divider />
+      <CardHeader
+        avatar={<AccountCircle></AccountCircle>}
+        title={
+          <Grid container justifyContent='space-between'>
+            <Typography variant="subtitle1">{discussion.author}</Typography>
+            <Typography variant="subtitle2">
+              <TimeAgo timestamp={discussion.created_utc} />
+            </Typography>
+          </Grid>
+        }
+      />
+      <CardContent>
+        <Grid container alignItems="flex-start">
+          <Typography textAlign='left'>{discussion.body}</Typography>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
